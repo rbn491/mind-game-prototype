eecfba4d737a9fddb16bf136244b7e95f75a8b32
Unknown <ruben.pferrer@hotmail.de>
2017-07-21 23:42:25 +0200

    UPDATE game mechanic

--- js/application.js
+++ js/application.js
@@ -1,45 +1,52 @@
 function init()
 {
-	var isPregame_ = true;
 	var itemCount_ = 3;
 	var pregameInterval_ = 5000;
-	var gameContainer_ = document.querySelector(".game-container .item-wrapper");
+	var gameContainer_ = document.querySelector(".game-container");
+	var itemWrapperShow_ = document.querySelector(".item-wrapper.show");
+	var itemWrapperPlay_ = document.querySelector(".item-wrapper.play");
+	var isPregame_ = gameContainer_.classList.contains("pregame");
 
 	var itemArr_ = [
 		"blue", "red", "green", "black", "purple",
 		"yellow", "grey", "pink", "lime", "lightblue"
 	];
 
+	var savedItems_, checkingArr_ =  [];
 
 
-	function getItemsFromArray_()
+
+
+	function getItemsFromArray_(count)
 	{
-		if(isPregame_)
-		{
-			var arr = shuffle(itemArr_);
+		var arr = shuffle(itemArr_);
 
-			for(var i = 0; i < itemCount_; i++)
+		if(count != null)
+		{
+			for(var i = 0; i < count; i++)
+			{
+				createItems_(arr[i]);
+				savedItems_.push(arr[i]);
+			}
+		}
+		else
+		{
+			for(var i = 0; i < itemArr_.length; i++)
 			{
 				createItems_(arr[i]);
 			}
 		}
 	}
 
-	function getRandomNumberFromArr_()
-	{
-		var number = Math.floor(Math.random() * itemArr_.length)
-		console.log(number);
 
-		return number;
-	}
+	// function getRandomNumberFromArr_()
+	// {
+	// 	var number = Math.floor(Math.random() * itemArr_.length)
+	// 	console.log(number);
 
-	function createItems_(param)
-	{
-		var item = document.createElement("div");
-		item.classList.add("game-item", param);
+	// 	return number;
+	// }
 
-		gameContainer_.appendChild(item);
-	}
 
 	/*
 	 * src: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?noredirect=1&lq=1
@@ -61,7 +68,51 @@ function init()
 		return array;
 	}
 
-	getItemsFromArray_();
+
+	function createItems_(param)
+	{
+		var item = document.createElement("div");
+		item.classList.add("game-item", param);
+		item.dataset.itemId = param;
+
+		itemWrapperShow_.appendChild(item);
+	}
+
+
+	function handleItemClick_()
+	{
+		var items = document.querySelectorAll("div.game-item");
+		var attr;
+
+		for(var i = 0; i < items.length; i++)
+		{
+			items[i].addEventListener("click", function()
+			{
+				attr = event.currentTarget.dataset.itemId;
+				console.log(attr);
+
+				checkingArr_.push(attr);
+				console.log(checkingArr_);
+			});
+		}
+	}
+
+
+	function constructGame_()
+	{
+		if(isPregame_)
+		{
+			getItemsFromArray_(itemCount_);
+			console.log(savedItems_);
+		}
+		else
+		{
+			getItemsFromArray_();
+			handleItemClick_();
+		}
+	}
+
+	constructGame_();
 }
 
 window.onload = init;
